4032730a4deac0d334214b7da5c2608705d2ccbe
user <user@DESKTOP-E44AMSG>
2025-09-05 15:55:44 +0900

    修正済み

--- self-introduction_1/src/Person/java/Person.java
+++ self-introduction_1/src/Person/java/Person.java
@@ -23,10 +23,10 @@ public double bmi () {
 public void print () {
 	System.out.println("名前は" + name + "です");
 	System.out.println("年は" + age + "です");
-	System.out.println("BMIは" + String.format("%.1f",this.bmi())  + "です");
+	System.out.println("BMIは" + Math.floor(this.bmi())  + "です");
 	System.out.println();
 	System.out.println("合計" + hito + "です");
 }
-	
-
 }
+//String.format("%.1f",this.bmi())　double型小数点第1まで表示　四捨五入して表示
+//Math.floor(this.bmi())  数値の小数点以下をすべて切り捨て、double 型で返します。﻿
